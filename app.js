be9d72292b7ee21cc6b2870747cf2b6170261612
Sara Wilcox <saraewilcox@gmail.com>
2020-11-03 20:54:49 +0000

    1st commit

--- app.js
+++ app.js
@@ -1,25 +1,33 @@
 const express = require('express');
-
 const hbs = require('hbs');
 const path = require('path');
 const PunkAPIWrapper = require('punkapi-javascript-wrapper');
-
 const app = express();
 const punkAPI = new PunkAPIWrapper();
-
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'views'));
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(__dirname + '/views/partials')
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+app.get('/beers', (req, res) => {
+  punkAPI
+  .getBeers()
+  .then(beersFromAPI => res.render('beers', {beersFromAPI}))
+  .catch(error => console.log(error));
+});
+app.get('/random-beers', (req, res) => {
+  punkAPI
+  .getRandom()
+  .then(randomBeer => res.render('random-beers', {randomBeer}))
+  .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
